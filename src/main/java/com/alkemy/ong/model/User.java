882d688a50432a74cd4fb0cafc6da039f76b6dfc
salvatierra1 <victordanielsalvatierra6@gmail.com>
2022-03-17 19:01:29 -0300

    OT167-17 - agregue roleId

--- src/main/java/com/alkemy/ong/model/User.java
+++ src/main/java/com/alkemy/ong/model/User.java
@@ -50,6 +50,16 @@ public class User {
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate renovationDate;
 
+    //Falta entidad Role
+    @ManyToMany
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(
+                    name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(
+                    name = "role_id", referencedColumnName = "id")
+    )
+    private Collection<Role> roles;
 }
 
 
