24c76c1c4f3ac3b43ac5859e4257bdf5cc983171
salvatierra1 <victordanielsalvatierra6@gmail.com>
2022-03-17 17:37:46 -0300

    OT167-17 - Creación de modelo y repositorio base de Users

---
+++ src/main/java/com/alkemy/ong/model/User.java
@@ -0,0 +1,55 @@
+package com.alkemy.ong.model;
+
+import com.sun.istack.NotNull;
+import com.sun.istack.Nullable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+@SQLDelete(sql = "UPDATE users SET deleted = true WHERE id=?")
+@Where(clause = "deleted = false")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private boolean deleted = Boolean.FALSE;
+
+    @NotNull
+    private String firstName;
+
+    @NotNull
+    private String lastName;
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String password;
+
+    @Nullable
+    private String photo;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate createDate;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate renovationDate;
+
+}
+
+
