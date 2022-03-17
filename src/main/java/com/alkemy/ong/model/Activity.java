08fbcce13e4682e0b3cb7478f9e0a8780d522ab8
Andres <rodrigueza.federacion@gmail.com>
2022-03-17 14:46:09 -0300

    OT167-21: creación de entidad actividad y repositorio actividad

---
+++ src/main/java/com/alkemy/ong/model/Activity.java
@@ -0,0 +1,31 @@
+package com.alkemy.ong.model;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.sun.istack.NotNull;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "activities")
+@Data @NoArgsConstructor @AllArgsConstructor
+public class Activity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private String name;
+    @NotNull
+    private String content;
+    @NotNull
+    private String image;
+    @CreationTimestamp
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
+    @Column(name = "creation_date")
+    private LocalDate creationDate;
+    @UpdateTimestamp
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
+    @Column(name = "update_date")
+    private LocalDate updateDate;
+}
