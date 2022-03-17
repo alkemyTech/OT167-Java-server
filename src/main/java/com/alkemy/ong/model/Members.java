98a40b91cabe3be96e8ef759035e84712ef05b02
Mickaela <tarazaga.mickaela@gmail.com>
2022-03-17 11:51:46 -0300

    Creation of the model and base repository of Members

---
+++ src/main/java/com/alkemy/ong/model/Members.java
@@ -0,0 +1,54 @@
+package com.alkemy.ong.model;
+
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.time.LocalDate;
+import javax.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "members")
+public class Members {
+    
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_member")
+    private Long idMember;
+
+    @NotNull
+    private String name;
+
+    @Column(name = "facebook_url")
+    private String facebookUrl;
+
+    @Column(name = "instagram_url")
+    private String instagramUrl;
+
+    @Column(name = "linkedin_url")
+    private String linkedinUrl;
+
+    @NotNull
+    private String image;
+
+    private String description;
+
+    @CreationTimestamp
+    @Column(name = "date_created")
+    private LocalDate dateCreated;
+
+    @UpdateTimestamp
+    @Column(name = "date_modified")
+    private LocalDate dateModified;
+
+    @Column(name = "is_active")
+    private Boolean isActive = Boolean.TRUE;
+
+   
+}
