6f192db8bf21ee03bed8abf457231a17f7a2aad1
agusgar409 <agusting409@gmail.com>
2022-03-18 00:01:51 -0300

    Create model and repository of Categories

---
+++ src/main/java/model/CategoriesEntity.java
@@ -0,0 +1,55 @@
+package model;
+
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.Where;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "categories")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+//chequear que esta bien cuando se realize softDelete de una categoria
+@SQLDelete(sql = "UPDATE categories SET deleted = true WHERE id_categories=?")
+@Where(clause = "deleted=false")
+public class CategoriesEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categories")
+    private Long idCategories;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String description;
+
+    @NotNull
+    private String image;
+
+    @Column(name = "deleted")
+    private boolean deleted = Boolean.FALSE;
+
+    @CreationTimestamp
+    @Column(name = "creation_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate creationDate;
+
+    @UpdateTimestamp
+    @Column(name = "update_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate updateDate;
+}
---
+++ src/main/java/repository/CategoriesRepository.java
@@ -0,0 +1,10 @@
+package repository;
+
+import model.CategoriesEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriesRepository extends JpaRepository<CategoriesEntity, Long> {
+
+}
