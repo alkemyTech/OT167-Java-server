276b8cd3b4856412fbe5bc54d4a0ad2b3180a686
Andres <rodrigueza.federacion@gmail.com>
2022-03-18 19:07:28 -0300

    feature/OT167-23: modifying organization controller and service

--- src/main/java/com/alkemy/ong/controller/OrganizationController.java
+++ src/main/java/com/alkemy/ong/controller/OrganizationController.java
@@ -1,10 +1,13 @@
 package com.alkemy.ong.controller;
+import com.alkemy.ong.dto.OrganizationDto;
+import com.alkemy.ong.model.Organization;
 import com.alkemy.ong.service.OrganizationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,8 +16,15 @@ public class OrganizationController {
     @Autowired
     private OrganizationService organizationService;
 
+    private OrganizationDto organizationDto;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<?> getAllOrganizations(@PathVariable Long id){
-        return ResponseEntity.ok(organizationService.getOrganizationById(id).get());
+        return ResponseEntity.ok(organizationDto.organizationEntityToDto(organizationService.getOrganizationById(id).get()));
+    }
+    @PostMapping(value = "/")
+    public ResponseEntity<?> createOrganization(@Valid @RequestBody Organization organization){
+        organizationService.createOrganization(organization);
+        return ResponseEntity.status(HttpStatus.CREATED).body(organizationDto.organizationEntityToDto(organization));
     }
 }
