c804dbdda05853d3d2b978c2dbe0494d790ec625
lucianoLattante <leprosoluciano@gmail.com>
2022-03-19 00:21:58 -0300

    OT167-16

--- src/main/java/com/alkemy/ong/model/Role.java
+++ src/main/java/com/alkemy/ong/model/Role.java
@@ -1,93 +1,49 @@
 package com.alkemy.ong.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.sun.istack.NotNull;
-import com.sun.istack.Nullable;
-import lombok.Data;
+
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.lang.Nullable;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @Entity
 @Table(name = "roles")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Role {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @NotNull
     @Column(name = "id")
+    @Getter @Setter
     private Long id;
 
     @Column(name = "name")
-    @NotNull
+    @Getter @Setter
+    @NotNull(message = "name can't be null")
     private String name;
 
-    @Column(name = "descripcion")
+    @Column(name = "description")
+    @Getter @Setter
     @Nullable
-    private String descripcion;
+    private String description;
 
     @Column(name = "CreatedDate", updatable=false)
     @CreationTimestamp
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    @Getter @Setter
     private LocalDateTime createdDate;
 
     @Column(name = "ModifiedDate")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
     @UpdateTimestamp
+    @Getter @Setter
     private LocalDateTime modifiedDate;
 
-    public Role() {
-    }
-
-    public Role(Long id, String name, String descripcion, LocalDateTime createdDate, LocalDateTime modifiedDate) {
-        this.id = id;
-        this.name = name;
-        this.descripcion = descripcion;
-        this.createdDate = createdDate;
-        this.modifiedDate = modifiedDate;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public LocalDateTime getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(LocalDateTime createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public LocalDateTime getModifiedDate() {
-        return modifiedDate;
-    }
-
-    public void setModifiedDate(LocalDateTime modifiedDate) {
-        this.modifiedDate = modifiedDate;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
 }
