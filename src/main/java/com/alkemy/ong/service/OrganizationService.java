75dc05d97fa35145ab09364d69b28448491b6dc2
Andres <rodrigueza.federacion@gmail.com>
2022-03-20 09:53:18 -0300

    OT167-23: modifying folders referring controller and service.

--- src/main/java/com/alkemy/ong/service/OrganizationService.java
+++ src/main/java/com/alkemy/ong/service/OrganizationService.java
@@ -4,5 +4,4 @@ import java.util.Optional;
 
 public interface OrganizationService {
     Optional<Organization> getOrganizationById(Long id);
-    Organization createOrganization(Organization organization);
 }
--- src/main/java/com/alkemy/ong/service/impl/OrganizationServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/OrganizationServiceImpl.java
@@ -19,8 +19,4 @@ public class OrganizationServiceImpl implements OrganizationService {
         if(organizationRepository.findById(id).isEmpty()) throw new NotFoundException("no one organization was add or created");
         return organizationRepository.findById(id);
     }
-    @Override
-    public Organization createOrganization(Organization organization) {
-        return organizationRepository.save(organization);
-    }
 }
