ccba255ded39bea95e8330fe48a9d3cfcf246006
Mickaela <tarazaga.mickaela@gmail.com>
2022-03-22 11:36:06 -0300

    I fix the conflict with the service created by a partner, leaving the class as interface and its implementation in the corresponding package

--- src/main/java/com/alkemy/ong/service/impl/UserServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/UserServiceImpl.java
@@ -1,12 +1,13 @@
 package com.alkemy.ong.service.impl;
 
-import java.util.Date;
 import com.alkemy.ong.dto.UserRegisterRequest;
 import com.alkemy.ong.dto.UserRegisterResponse;
 import com.alkemy.ong.exception.DataAlreadyExistException;
+import com.alkemy.ong.mapper.UserMapper;
 import com.alkemy.ong.model.User;
 import com.alkemy.ong.repository.UserRepository;
 import com.alkemy.ong.service.UserService;
+import java.time.LocalDate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -19,7 +20,15 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     PasswordEncoder passwordEncoder;
-      
+
+    @Autowired
+    UserMapper userMapper;
+
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
     @Override
     public User findByEmail(String email) {
         return userRepository.findByEmail(email);
@@ -31,14 +40,11 @@ public class UserServiceImpl implements UserService {
         if (this.findByEmail(userReq.getEmail()) != null) {
             throw new DataAlreadyExistException("This email is already registered");
         }
-
-        // Falta Asignar Rol
-        User user = UserRegisterRequest.mapToEntity(userReq);
+        //Falta asignar rol
+        User user = userMapper.UserRegisterDto2User(userReq);
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setCreationDate(new Date());
-
-        return UserRegisterResponse.mapToResponse(userRepository.save(user));
+        user.setCreationDate(LocalDate.now());
+        return userMapper.User2UserRegisterDto(userRepository.save(user));
     }
 
-    
 }
