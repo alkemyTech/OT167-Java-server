5b94eeab8820fc1e559ea33a2be141563594890a
Mickaela <tarazaga.mickaela@gmail.com>
2022-03-22 11:34:14 -0300

    I fix the conflict with the service created by a partner, leaving the class as interface and its implementation in the corresponding package

--- src/main/java/com/alkemy/ong/service/UserService.java
+++ src/main/java/com/alkemy/ong/service/UserService.java
@@ -1,33 +1,17 @@
 package com.alkemy.ong.service;
 
-<<<<<<< HEAD
 import java.io.IOException;
 import com.alkemy.ong.dto.UserRegisterRequest;
 import com.alkemy.ong.dto.UserRegisterResponse;
 import com.alkemy.ong.exception.DataAlreadyExistException;
+import com.alkemy.ong.model.User;
 
 
 public interface UserService {
 
+    User findByUsername(String username);
     User findByEmail(String email);
     UserRegisterResponse register(UserRegisterRequest userReq) throws DataAlreadyExistException, IOException;
-     
+    
 
-
-=======
-import org.springframework.beans.factory.annotation.Autowired;
-import com.alkemy.ong.model.User;
-import com.alkemy.ong.repository.UserRepository;
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserService {
-
-    @Autowired
-    private UserRepository userRepository;
-
-    public User findByUsername(String username) {
-        return userRepository.findByUsername();
-    }
->>>>>>> develop
 }
