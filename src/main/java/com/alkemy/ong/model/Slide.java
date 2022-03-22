92fc6ed7d88fb78692f3abd3b4455feb2ff8fa73
Andres <rodrigueza.federacion@gmail.com>
2022-03-22 20:50:40 -0300

    OT167-23: changes for avoid conflids

---
+++ src/main/java/com/alkemy/ong/model/Slide.java
@@ -0,0 +1,33 @@
+package com.alkemy.ong.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "slides")
+public class Slide {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String imageUrl;
+
+    private String text;
+
+    @Column(name = "order_number")
+    private Integer order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "organization_id")
+    private Organization organization;
+
+}
