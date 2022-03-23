4226b8b6a48a307503644679c24587f81afc63fd
Brito Maria Luz <britomluz@gmail.com>
2022-03-23 08:47:36 -0300

    GetMapping list of users

--- src/main/java/com/alkemy/ong/security/controller/UserAuth.java
+++ src/main/java/com/alkemy/ong/security/controller/UserAuth.java
@@ -1,15 +1,22 @@
 package com.alkemy.ong.security.controller;
 
+import com.alkemy.ong.dto.UserDto;
 import com.alkemy.ong.mapper.UserMapper;
+import com.alkemy.ong.model.Role;
+import com.alkemy.ong.model.User;
 import com.alkemy.ong.security.service.JwtUtils;
 import com.alkemy.ong.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/auth")
@@ -38,4 +45,20 @@ public class UserAuth {
 
         return ResponseEntity.ok(userMapper.convertUserToDto(userService.findByUsername(username)));
     }
+
+    @GetMapping("/users")
+    public ResponseEntity<?> getUsuarios(Authentication authentication){
+
+        User userAuth = userService.findByEmail(authentication.getName());
+        Role role = userService.getRole("ROLE_ADMIN");
+
+        if (!userAuth.getRoles().contains(role)){
+            return new ResponseEntity<>("Not allowed", HttpStatus.METHOD_NOT_ALLOWED);
+        }
+
+        List<UserDto> users = userService.getUsers().stream().map(user -> userMapper.convertUserToDto(user)).collect(Collectors.toList());
+
+        return ResponseEntity.ok().body(users);
+    }
+
 }
--- src/main/java/com/alkemy/ong/service/UserService.java
+++ src/main/java/com/alkemy/ong/service/UserService.java
@@ -1,9 +1,13 @@
 package com.alkemy.ong.service;
 
+import com.alkemy.ong.model.Role;
+import com.alkemy.ong.repository.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.alkemy.ong.model.User;
 import com.alkemy.ong.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -11,7 +15,24 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public User findByUsername(String username) {
-        return userRepository.findByUsername();
+    @Autowired
+    private RoleRepository roleRepository;
+
+    public User findByUsername(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public List<User> getUsers(){
+        return userRepository.findAll();
+    }
+
+    public User findByEmail(String email)
+    {
+        return userRepository.findByEmail(email);
+    }
+
+    public Role getRole(String name) {
+
+        return roleRepository.findByName(name);
     }
 }
