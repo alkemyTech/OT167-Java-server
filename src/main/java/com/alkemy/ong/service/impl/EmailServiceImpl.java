f733674990a94f14bb2cd7406797b28da393ee96
Pablo Ibañez <pablomat91@gmail.com>
2022-03-23 23:50:33 -0300

    Cambios finales

--- src/main/java/com/alkemy/ong/service/impl/EmailServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/EmailServiceImpl.java
@@ -11,15 +11,11 @@ import com.sendgrid.SendGrid;
 import com.sendgrid.helpers.mail.Mail;
 import com.sendgrid.helpers.mail.objects.Content;
 import com.sendgrid.helpers.mail.objects.Email;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
-import org.springframework.web.servlet.ModelAndView;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
@@ -31,15 +27,12 @@ public class EmailServiceImpl implements EmailService {
     @Value("${alkemy.ong.email.sender}")
     private String emailSender;
 
-//    private static final String TEMPLATE_PATH = "/src/main/resources/template/plantilla_email.html";
-
     @Autowired
     private TemplateEngine templateEngine;
 
     public void sendWelcomeEmailTo(String to) {
-//        String apiKey = env.getProperty("EMAIL_API_KEY");
-//        SendGrid sg = new SendGrid(apiKey);
-        SendGrid sg = new SendGrid("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");
+        String apiKey = env.getProperty("EMAIL_API_KEY");
+        SendGrid sg = new SendGrid(apiKey);
 
         try {
             Request request = this.buildRequest(to);
@@ -62,22 +55,6 @@ public class EmailServiceImpl implements EmailService {
 
     }
 
-//    public String getContentOfHtmlTemplate() {
-//        String filePath = new File("").getAbsolutePath() + TEMPLATE_PATH;
-//        StringBuilder contentBuilder = new StringBuilder(); 
-//        try {
-//            BufferedReader in = new BufferedReader(new FileReader(filePath));
-//            String str;
-//            while ((str = in.readLine()) != null) {
-//                contentBuilder.append(str);
-//            }
-//            in.close();
-//        } catch (IOException e) {
-//            UtilsLog.registrarInfo(EmailService.class, ERROR, "Error" + e);
-//        }
-//        String content = contentBuilder.toString();
-//        return content;
-//    }
     public Mail buildMail(String to) {
 
         Content content = new Content("text/html", preparedWelcomeBodyEmail());
@@ -98,12 +75,4 @@ public class EmailServiceImpl implements EmailService {
 
         return request;
     }
-
-//    public ModelAndView loadData() {
-//
-//        ModelAndView model = new ModelAndView("plantilla_html");
-//        model.addObject("contact", "Lavalle 1856");
-//
-//        return model;
-//    }
 }
