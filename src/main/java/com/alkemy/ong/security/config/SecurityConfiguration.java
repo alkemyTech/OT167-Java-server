351e8bfedcdcf7a9aed6255c6dc70fea395a3327
Mickaela <tarazaga.mickaela@gmail.com>
2022-03-25 12:53:42 -0300

    I modify the path to authorize anyone matching /auth, which is the correct path for registration and login

--- src/main/java/com/alkemy/ong/security/config/SecurityConfiguration.java
+++ src/main/java/com/alkemy/ong/security/config/SecurityConfiguration.java
@@ -1,7 +1,7 @@
 package com.alkemy.ong.security.config;
 
 import com.alkemy.ong.security.filter.JwtRequestFilter;
-import com.alkemy.ong.security.service.UserDetailsService;
+import com.alkemy.ong.security.service.UserDetailsCustomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,7 +18,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private UserDetailsService userDetailsService;
+    private UserDetailsCustomService userDetailsService;
 
     @Autowired
     private JwtRequestFilter jwtRequestFilter;
@@ -42,7 +42,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws  Exception {
         http.csrf().disable()
-                .authorizeRequests().antMatchers("/authenticate").permitAll().
+                .authorizeRequests().antMatchers("/auth/**").permitAll().
                 anyRequest().authenticated()
                 .and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
