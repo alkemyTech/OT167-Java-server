3124ae670e5c2c94e55d3cafc708300913ddeaeb
Andres <rodrigueza.federacion@gmail.com>
2022-03-27 16:25:29 -0300

    OT167-62: modifying layers to match the layers created by the co-worker

--- src/main/java/com/alkemy/ong/controller/ContactController.java
+++ src/main/java/com/alkemy/ong/controller/ContactController.java
@@ -1,8 +1,6 @@
 package com.alkemy.ong.controller;
-import com.alkemy.ong.dto.ContactCreationDto;
+import com.alkemy.ong.dto.ContactDto;
 import com.alkemy.ong.exception.MessageInfo;
-import com.alkemy.ong.mapper.ContactMapper;
-import com.alkemy.ong.model.Contact;
 import com.alkemy.ong.service.ContactService;
 import com.alkemy.ong.service.EmailService;
 import lombok.RequiredArgsConstructor;
@@ -10,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 
@@ -29,17 +24,17 @@ public class ContactController {
     private final MessageSource messageSource;
     @Autowired
     private ContactService contactService;
-    @Autowired
-    private ContactMapper contactMapper;
 
     @PostMapping("/register")
-    public ResponseEntity<?> contact(@Valid @RequestBody ContactCreationDto contactDto, WebRequest request) {
-        Contact contact = contactMapper.creationContFromContactDto(contactDto);
-        contactService.saveContact(contact);
-            emailService.sendEmail(
-                    messageSource.getMessage("email.subject", new Object[]{contact.getName()}, Locale.ENGLISH),
-                    contact.getEmail(),
-                    messageSource.getMessage("email.message",null, Locale.ENGLISH));
-        return ResponseEntity.status(HttpStatus.CREATED).body(new MessageInfo(messageSource.getMessage("contact.registered.successfully", null, Locale.ENGLISH), HttpStatus.CREATED.value(), ((ServletWebRequest)request).getRequest().getRequestURI()));
+    public ResponseEntity<?> contact(@Valid @RequestBody ContactDto contactDto, WebRequest request) {
+        contactService.save(contactDto);
+        emailService.sendEmail(
+                messageSource.getMessage("email.subject", new Object[]{contactDto.getName()}, Locale.ENGLISH),
+                contactDto.getEmail(),
+                messageSource.getMessage("email.message",null, Locale.ENGLISH));
+        return ResponseEntity.status(HttpStatus.CREATED).body(new MessageInfo(
+                messageSource.getMessage("contact.registered.successfully",null, Locale.ENGLISH),
+                HttpStatus.CREATED.value(),
+                ((ServletWebRequest)request).getRequest().getRequestURI()));
     }
 }
--- src/main/java/com/alkemy/ong/service/impl/ContactServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/ContactServiceImpl.java
@@ -1,22 +1,26 @@
 package com.alkemy.ong.service.impl;
-
-
+import com.alkemy.ong.dto.ContactDto;
+import com.alkemy.ong.mapper.ContactMapper;
 import com.alkemy.ong.model.Contact;
 import com.alkemy.ong.repository.ContactRespository;
 import com.alkemy.ong.service.ContactService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
-@Slf4j
 public class ContactServiceImpl implements ContactService {
     private final ContactRespository contactRespository;
+    @Autowired
+    private ContactMapper contactMapper;
     @Override
-    public Contact saveContact(Contact contact) {
-        return contactRespository.save(contact);
+    public ContactDto save(ContactDto dto) {
+        Contact contact = contactMapper.contactDto2Contact(dto);
+        Contact contactSaved = contactRespository.save(contact);
+        ContactDto result = contactMapper.contacto2ContactDto(contactSaved);
+        return result;
     }
 }
