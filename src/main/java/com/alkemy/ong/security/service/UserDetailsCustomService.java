b0152ae347b0e2a16e341f9234c7b521b322262a
mickaelatarazaga <94411297+mickaelatarazaga@users.noreply.github.com>
2022-03-28 12:08:16 -0300

    Modify the loadUserByUsername method

--- src/main/java/com/alkemy/ong/security/service/UserDetailsCustomService.java
+++ src/main/java/com/alkemy/ong/security/service/UserDetailsCustomService.java
@@ -1,25 +1,23 @@
 package com.alkemy.ong.security.service;
 import com.alkemy.ong.security.model.UserEntity;
 import com.alkemy.ong.exception.DataAlreadyExistException;
-import org.springframework.security.core.userdetails.UserDetails;
-
 import com.alkemy.ong.repository.UserRepository;
 import com.alkemy.ong.security.dto.UserRegisterRequest;
 import com.alkemy.ong.security.dto.UserRegisterResponse;
 import com.alkemy.ong.security.mapper.UserMapper;
 import com.alkemy.ong.service.UserService;
+import java.util.Collections;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Locale;
-import java.util.stream.Collectors;
 import org.springframework.context.MessageSource;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @Service
 public class UserDetailsCustomService implements UserDetailsService {
@@ -36,12 +34,6 @@ public class UserDetailsCustomService implements UserDetailsService {
     @Autowired
     private MessageSource messageSource;
 
-    @Override
-    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-        return new User("foo", "foo", new ArrayList<>());
-    }
-    
-        
     public UserRegisterResponse register(UserRegisterRequest userReq) throws DataAlreadyExistException {
 
         if (userService.findByEmail(userReq.getEmail()) != null) {
@@ -53,8 +45,13 @@ public class UserDetailsCustomService implements UserDetailsService {
              
     }
 
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByEmail(email);
+        User userDet = new User(user.getEmail(), user.getPassword(), Collections.emptyList()); 
+        return userDet;}
 
-}
+ }
 
 
 
