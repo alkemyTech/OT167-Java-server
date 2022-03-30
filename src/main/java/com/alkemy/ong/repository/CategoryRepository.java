4560c7be4c9a73c9989b53e6afc44bb6ee9d0b64
lucianoLattante <leprosoluciano@gmail.com>
2022-03-29 23:29:25 -0300

    fixing errors

--- src/main/java/com/alkemy/ong/repository/CategoryRepository.java
+++ src/main/java/com/alkemy/ong/repository/CategoryRepository.java
@@ -8,8 +8,4 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     Category findByName(String name);
 
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> feature/OT167-43
--- src/main/java/com/alkemy/ong/security/controller/UserAuthController.java
+++ src/main/java/com/alkemy/ong/security/controller/UserAuthController.java
@@ -14,15 +14,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.io.IOException;
 import java.util.List;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/auth")
