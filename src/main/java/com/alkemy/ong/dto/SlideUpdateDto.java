29067f8cfceb9327877dfe30e8575e00596e24e6
Andres <rodrigueza.federacion@gmail.com>
2022-04-02 23:54:57 -0300

    OT167-58: improving the Slide dto

--- src/main/java/com/alkemy/ong/dto/SlideUpdateDto.java
+++ src/main/java/com/alkemy/ong/dto/SlideUpdateDto.java
@@ -1,11 +1,7 @@
 package com.alkemy.ong.dto;
-
-import com.alkemy.ong.model.Organization;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-
-import javax.persistence.Column;
 
 @Getter
 @Setter
@@ -13,7 +9,6 @@ import javax.persistence.Column;
 public class SlideUpdateDto {
     private String imageUrl;
     private String text;
-    @Column(name = "order_number")
     private Integer order;
     private String orgName;
 }
