afff9f171edcbcfd8aa58d9d227ff3e7719e6e92
Facundo Villalba <villalbafacundo@hotmail.com>
2022-04-06 13:14:25 -0300

    Initial commit, testimonial put by id

--- src/main/java/com/alkemy/ong/mapper/TestimonialMapper.java
+++ src/main/java/com/alkemy/ong/mapper/TestimonialMapper.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class TestimonialMapper {
-    
+
     public Testimonial testimonialDto2Entity (TestimonialDto testimonialDto){ 
         Testimonial testimonial = new Testimonial();
         testimonial.setName(testimonialDto.getName());
@@ -16,6 +16,21 @@ public class TestimonialMapper {
         testimonial.setCreationDate(LocalDate.now());
         return testimonial;
     }
-           
-            
+
+
+    public TestimonialDto testimonial2DTO (Testimonial testimonial){
+        TestimonialDto testimonialDto = new TestimonialDto();
+        testimonialDto.setId(testimonial.getId());
+        testimonialDto.setName(testimonial.getName());
+        testimonialDto.setImage(testimonial.getImage());
+        testimonialDto.setContent(testimonial.getContent());
+
+        return testimonialDto;
+    }
+
+    public void activityRefreshValues(Testimonial testimonial, TestimonialDto testimonialDto) {
+        testimonial.setName(testimonialDto.getName());
+        testimonial.setImage(testimonialDto.getImage());
+        testimonial.setContent(testimonialDto.getContent());
+    }
 }
