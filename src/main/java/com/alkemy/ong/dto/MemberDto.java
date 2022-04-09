fbab7da81d09435e92ac8166dbeae6a8383142d5
Andres <rodrigueza.federacion@gmail.com>
2022-04-09 20:33:07 -0300

    OT167-87: i was improving the messages

--- src/main/java/com/alkemy/ong/dto/MemberDto.java
+++ src/main/java/com/alkemy/ong/dto/MemberDto.java
@@ -1,4 +1,5 @@
 package com.alkemy.ong.dto;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -9,10 +10,12 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 public class MemberDto {
         private Long id;
+        @Schema(nullable = false, description = "name: cannot allow null")
         private String name;
         private String facebookUrl;
         private String instagramUrl;
         private String linkedinUrl;
+        @Schema(nullable = false, description = "image: cannot allow null")
         private String image;
         private String description;
         private LocalDateTime creationDate;
