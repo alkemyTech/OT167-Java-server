af0208f319a419d9db5f0f527a304733a6df7d57
agusgar409 <62307436+agusgar409@users.noreply.github.com>
2022-04-11 08:20:26 -0300

    uncomments News and User verification, this was only for testing purposes.

--- src/main/java/com/alkemy/ong/mapper/CommentMapper.java
+++ src/main/java/com/alkemy/ong/mapper/CommentMapper.java
@@ -41,18 +41,18 @@ public class CommentMapper {
 
         CommentDto commentValidated= Validate(commentDto);
 
-        /*if(commentValidated.getNews_id()==null){
+        if(commentValidated.getNews_id()==null){
             throw new NullPointerException(messageSource.getMessage("news.not.null", null, Locale.ENGLISH));
         }
 
         if(commentValidated.getUser_id()==null){
             throw new NullPointerException(messageSource.getMessage("user.not.found", null, Locale.ENGLISH));
-        }*/
+        }
 
         Comment newComment = new Comment();
 
-        //newComment.setNews_id(newsService.findNewById(commentValidated.getNews_id()).get());
-        //newComment.setUser_id(userService.findUserById(commentValidated.getUser_id()).get());
+        newComment.setNews_id(newsService.findNewById(commentValidated.getNews_id()).get());
+        newComment.setUser_id(userService.findUserById(commentValidated.getUser_id()).get());
         newComment.setBody(commentDto.getBody());
         return newComment;
 
@@ -60,13 +60,13 @@ public class CommentMapper {
 
     private CommentDto Validate(CommentDto commentDto){
         try{
-            //UserEntity userEntity= userService.findUserById(commentDto.getUser_id()).get();
+            UserEntity userEntity= userService.findUserById(commentDto.getUser_id()).get();
 
-            //News newsEntity = newsService.findNewById(commentDto.getNews_id()).get();
+            News newsEntity = newsService.findNewById(commentDto.getNews_id()).get();
 
-            //commentDto.setUser_id(userEntity.getId());
+            commentDto.setUser_id(userEntity.getId());
 
-            //commentDto.setNews_id(newsEntity.getId());
+            commentDto.setNews_id(newsEntity.getId());
 
             return commentDto;
         }catch (NoSuchElementException e){
