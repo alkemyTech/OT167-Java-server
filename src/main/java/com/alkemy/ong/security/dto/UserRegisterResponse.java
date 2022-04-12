178ee52882ad70955c79b087655bcec188f03a90
Mickaela <tarazaga.mickaela@gmail.com>
2022-04-12 20:55:12 -0300

    I add the documentation with Swagger Authentication

--- src/main/java/com/alkemy/ong/security/dto/UserRegisterResponse.java
+++ src/main/java/com/alkemy/ong/security/dto/UserRegisterResponse.java
@@ -1,6 +1,7 @@
 package com.alkemy.ong.security.dto;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Getter
@@ -9,10 +10,15 @@ import lombok.*;
 @AllArgsConstructor
 public class UserRegisterResponse {
 
+    @Schema(name = "id", type = "Long", example = "1")
     private Long id;
+    @Schema(name = "firstName", example = "Mickaela", type = "String")
     private String firstName;
+    @Schema(name = "lastName", example = "Tarazaga", type = "String")
     private String lastName;
+    @Schema(name = "email", example = "tarazaga.mickaela@gmail.com", type = "String")
     private String email;
+    @Schema(name = "jwt", example = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0YXJhemFnYS5taWNrYWVsYUBnbWFpbC5jb20iLCJleHAiOjE2NDg3NzMyNTgsImlhdCI6MTY0ODczNzI1OH0.yA07ci-rUbAvfoIAB4jeqQwl8lg1NOabLLcCZFLZuis", type = "String")
     private String jwt;
 
 }
