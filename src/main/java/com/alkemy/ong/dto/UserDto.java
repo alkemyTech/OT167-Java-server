773ee00c5096cd71562c5576b0d4305184fc643c
Mickaela <tarazaga.mickaela@gmail.com>
2022-04-12 20:51:54 -0300

    Dtos corresponding to the login with modifications to document with Swagger

--- src/main/java/com/alkemy/ong/dto/UserDto.java
+++ src/main/java/com/alkemy/ong/dto/UserDto.java
@@ -1,5 +1,6 @@
 package com.alkemy.ong.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Getter
@@ -9,9 +10,15 @@ import lombok.*;
 @NoArgsConstructor
 public class UserDto {
 
+    @Schema(name = "id", type = "Long", example = "1")
+    private Long id;
+    @Schema(name = "firstName", example = "Mickaela", type = "String")
     private String firstName;
+    @Schema(name = "lastName", example = "Tarazaga", type = "String")
     private String lastName;
+    @Schema(name = "email", example = "tarazaga.mickaela@gmail.com", type = "String")
     private String email;
+    @Schema(name = "photo", example = "url.com/image.jpg", type = "String")
     private String photo;
 
 }
---
+++ src/main/java/com/alkemy/ong/security/dto/UserLoginRequest.java
@@ -0,0 +1,25 @@
+package com.alkemy.ong.security.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+public class UserLoginRequest {
+
+    @Schema(example = "tarazaga.mickaela@gmail.com", required = true)
+    @NotBlank(message = "Email can't be blank")
+    @Email(message = "Email should be valid.", regexp = "[A-Za-z0-9._%-+]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}")
+    private String email;
+
+    @Schema(name = "password", example = "password123", type = "String", required = true)
+    @NotBlank(message = "Password is mandatory.")
+    @Size(min = 6, message = "The password must be at least 6 characters")
+    private String password;
+
+}
