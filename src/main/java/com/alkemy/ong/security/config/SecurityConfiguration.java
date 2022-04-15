b3af856261b6e4975bae0605f06846595b1df173
Mickaela <tarazaga.mickaela@gmail.com>
2022-04-15 14:31:55 -0300

    Endpoint user validation

--- src/main/java/com/alkemy/ong/security/config/SecurityConfiguration.java
+++ src/main/java/com/alkemy/ong/security/config/SecurityConfiguration.java
@@ -13,9 +13,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.http.HttpMethod;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
-
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -32,7 +32,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
@@ -43,18 +43,39 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws  Exception {
-        http.csrf().disable()
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeRequests()
-                .antMatchers("/auth/*").permitAll()
-                .antMatchers("/api/docs/**","/api/swagger-ui/**","/v3/api-docs/**","/swagger-ui/**").permitAll()
-                .antMatchers("/categories/*").permitAll()
-                .anyRequest().authenticated()
-                .and().sessionManagement()
-                .sessionCreationPolicy(STATELESS);
-        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+                .antMatchers("/auth/login","/auth/register" ).permitAll() // Any user can access /auth/login and /auth/register
+                .antMatchers("/auth/me").hasAuthority("ROLE_ADMIN") // Only Admins can access other locations in /auth/**
+                .antMatchers(HttpMethod.GET,
+                        "/activities",
+                        "/organizations",
+                        "/categories",
+                        "/news",
+                        "/news/{id}").hasAnyAuthority("ROLE_ADMIN", "ROLE_USER") // Only authenticated roles can access GET methods
+                .antMatchers(HttpMethod.PUT,
+                        "/members" + "/**").hasAnyAuthority("ROLE_ADMIN", "ROLE_USER")
+                .antMatchers(HttpMethod.POST, "/organizations/public/**").hasAuthority("ROLE_ADMIN")
+                .antMatchers(HttpMethod.DELETE).hasAuthority("ROLE_ADMIN")
+                .antMatchers("/api/docs/**", "swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/api/swagger-ui/**").permitAll()
+                .antMatchers("/activities", "/activities/{id}",
+                        "/organizations",
+                        "/categories", "/categories/{id}",
+                        "/members", "/members/{id}",
+                        "/news", "/news/{id}",
+                        "/slides", "/slides/{id}",
+                        "/testimonials", "/testimonials/{id}",
+                        "/contacts", "/contacts/{id}",
+                        "/users").hasAuthority("ROLE_ADMIN")
+                // Only admins can access other methods
+                .antMatchers("/public/**").permitAll() // All users can access endpoints in /public/**
+                .anyRequest().authenticated() // Only authenticated users can access the rest of endpoints
+                .and()
+                .exceptionHandling()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
+
 }
-
-
-
