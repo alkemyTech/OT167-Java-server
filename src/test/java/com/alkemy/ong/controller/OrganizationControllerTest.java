b72aebaa07ac034033dfb44d2e7e8b492cd2ce57
lucianoLattante <leprosoluciano@gmail.com>
2022-04-16 01:54:46 -0300

    fixing unnused imports

--- src/test/java/com/alkemy/ong/controller/OrganizationControllerTest.java
+++ src/test/java/com/alkemy/ong/controller/OrganizationControllerTest.java
@@ -12,14 +12,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.*;
 
