ee925af5154148b4106fbb535456f9dd5d05af19
agusgar409 <62307436+agusgar409@users.noreply.github.com>
2022-04-17 13:17:08 -0300

    test creation activity

--- src/test/java/com/alkemy/ong/ActivityControllerTest.java
+++ src/test/java/com/alkemy/ong/ActivityControllerTest.java
@@ -1,9 +1,9 @@
 package com.alkemy.ong;
 
 import com.alkemy.ong.dto.ActivityDto;
+import com.alkemy.ong.mapper.ActivityMapper;
 import com.alkemy.ong.model.Activity;
 import com.alkemy.ong.service.ActivityService;
-import jdk.net.SocketFlow;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,13 +15,15 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import static org.mockito.BDDMockito.given;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -38,32 +40,60 @@ public class ActivityControllerTest {
     @MockBean
     private ActivityService activityService;
 
+    @MockBean
+    private ActivityMapper activityMapper;
+
     private ArrayList<Activity> activities = new ArrayList<>();
 
     private ActivityDto activityDto;
+    private ActivityDto activityDto2;
     private Activity activity;
+    private ActivityDto activityDtoResponse;
 
     @BeforeEach
     void setUp() {
         mockMvc = MockMvcBuilders.webAppContextSetup(context)
                 .apply(springSecurity())
                 .build();
-        Activity activity = new Activity(Long.valueOf(1),"name1","content1","image",null,null);
+        Activity activity = new Activity(Long.valueOf(1),"name1","content1",null,null,null);
         activities.add(activity);
 
+        ActivityDto activityDtoResponse = new ActivityDto();
+
         ActivityDto activityDto = new ActivityDto();
-        activityDto.setId(1L);
         activityDto.setName("name2");
         activityDto.setContent("content2");
-        activityDto.setImage("image");
+
+        ActivityDto activityDto2 = new ActivityDto();
+        activityDto2.setId(1L);
+        activityDto2.setName("name2");
+        activityDto2.setContent("content2");
+        activityDto2.setImage("image");
+
+
     }
 
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    void createAnActivity() throws Exception{
+
+        activityDtoResponse = activityMapper.activityToDTO(activity);
+
+        when(activityService.createActivity(activityDto, null)).thenReturn(activityDtoResponse);
+
+        mockMvc.perform(post("/activity/activities")
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(activityDtoResponse))
+                        .with(user("admin").roles("ADMIN"))
+                        .with(csrf()))
+                .andExpect(status().isOk());
+    }
 
     @Test
     @WithMockUser(roles = "ADMIN")
     void anActivityInformationIsUpdatedCorrectly() throws Exception {
         Long id = 1L;
-        when(activityService.update(id,activityDto)).thenReturn(activityDto);
+        when(activityService.update(id,activityDto2)).thenReturn(activityDto);
 
         this.mockMvc.perform(put("/activity/{id}",id))
                 .andExpect(status().is(200));
