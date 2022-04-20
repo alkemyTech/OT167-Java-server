6c795d62a9e4a3b65410bf0b74f4c55e6e40ef96
agusgar409 <62307436+agusgar409@users.noreply.github.com>
2022-04-19 23:38:28 -0300

    agrego schemas a los DTO que se me asignarion, y unifico las constantes de los controllers en una misma clase

--- src/main/java/com/alkemy/ong/controller/TestimonialController.java
+++ src/main/java/com/alkemy/ong/controller/TestimonialController.java
@@ -9,6 +9,7 @@ import java.util.Locale;
 import java.util.Map;
 import javax.validation.Valid;
 
+import com.alkemy.ong.utils.SwaggerConstants;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
@@ -31,15 +32,6 @@ import org.springframework.web.context.request.WebRequest;
 @RequestMapping("/testimonials")
 public class TestimonialController {
 
-    private final String MODEL_TESTIMONIAL = "{\"id\" : \"1\", \"name\" : \"Agostina Suarez\", \"image\" : \"imageUrl\", \"content\" : \"testimonial content\" ,\n" + "  \"creationDate\": \"2022-04-09T15:13:18.617Z\",\n" + "  \"updateDate\": \"2022-04-09T15:13:18.618Z\"}\n";
-    private final String MODEL_MESSAGE_PAGE = "{\n" + "  \"content\": [\n" + MODEL_TESTIMONIAL + "  ],\n" + "  \"status_code\": 200,\n" + "  \"nextPath\": \"/testimonials?page=2\",\n" + "  \"prevPath\": \"/testimonials?page=0\"\n" + "}";
-    private final String MODEL_TESTIMONIAL_PARAM_ERROR = "{\n" + "  \"message\": {\n" + " \"image\": \"Image cannot be null\"" + "  },\n" + "  \"status_code\": 400,\n" + "  \"path\": \"/testimonials\"\n" + "}";
-    private final String MODEL_TESTIMONIAL_EMPTY_ERROR = "{\n" + "  \"content\": [\n" + " \"There is a empty page.\"" + "  ],\n" + "  \"status_code\": 200,\n" + "  \"nextPath\": \"there's not page next.\",\n" + "  \"prevPath\": \"there's not page prev.\"\n" + "}";
-    private final String MODEL_TESTIMONIAL_ERROR_400 = "{\n" + "  \"message\": \"The character entered on the path is not a number\",\n" + "  \"status_code\": 400,\n" + "  \"path\": \"/testimonials\"\n" + "}";
-    private final String MODEL_TESTIMONIAL_ERROR_404 = "{\n" + "  \"message\": \"The testimonial is not found.\",\n" + "  \"status_code\": 404,\n" + "  \"path\": \"/testimonials\"\n" + "}";
-    private final String MODEL_TESTIMONIAL_CREATED = "{\n" + "  \"message\": \"The testimonial was created successfully.\",\n" + "  \"status_code\": 201,\n" + "  \"path\": \"/testimonials\"\n" + "}";
-    private final String MODEL_TESTIMONIAL_DELETE = "{\n" + "  \"message\": \"The testimonial was deleted.\",\n" + "  \"status_code\": 200,\n" + "  \"path\": \"/testimonials\"\n" + "}";
-
     @Autowired
     private MessageSource messageSource;
 
@@ -55,13 +47,13 @@ public class TestimonialController {
                             schema = @Schema(implementation = MessageInfo.class),
                             array = @ArraySchema(schema = @Schema(implementation = String.class)),
                             examples = {
-                                @ExampleObject(name = "Example 1", summary = "Testimonial created", description = "when the required fields are filled in, sends a testimonial message successfully created", value = MODEL_TESTIMONIAL_CREATED),})}),
+                                @ExampleObject(name = "Example 1", summary = "Testimonial created", description = "when the required fields are filled in, sends a testimonial message successfully created", value = SwaggerConstants.MODEL_TESTIMONIAL_CREATED),})}),
         @ApiResponse(responseCode = "400", description = "A param error",
                 content = {
                     @Content(mediaType = "application/json",
                             schema = @Schema(implementation = MessagesInfo.class),
                             examples = {
-                                @ExampleObject(name = "Example 1", summary = "Member param Error", description = "when required fields are not filled in, sends a 400 (Bad Request) error message", value = MODEL_TESTIMONIAL_PARAM_ERROR),})})})
+                                @ExampleObject(name = "Example 1", summary = "Member param Error", description = "when required fields are not filled in, sends a 400 (Bad Request) error message", value = SwaggerConstants.MODEL_TESTIMONIAL_PARAM_ERROR),})})})
     @PostMapping
     public ResponseEntity<Map<String, String>> saveTestimonial(@Valid @RequestBody TestimonialDto testimonialDto) {
         testimonialService.save(testimonialDto);
@@ -81,20 +73,20 @@ public class TestimonialController {
                     @Content(mediaType = "application/json",
                             schema = @Schema(implementation = TestimonialDto.class),
                             examples = {
-                                @ExampleObject(name = "Testimonial was update", summary = "Testimonial update", description = "when the required fields are completed, the user is updated and the entity update is sent.", value = MODEL_TESTIMONIAL
+                                @ExampleObject(name = "Testimonial was update", summary = "Testimonial update", description = "when the required fields are completed, the user is updated and the entity update is sent.", value = SwaggerConstants.MODEL_TESTIMONIAL
                                 )})}),
         @ApiResponse(responseCode = "400", description = "A param cannot be null",
                 content = {
                     @Content(mediaType = "application/json",
                             schema = @Schema(implementation = MessagesInfo.class),
                             examples = {
-                                @ExampleObject(name = "Example 1", summary = "Testimonial param Error", description = "when required fields are not filled in, sends a 400 (Bad Request) error message", value = MODEL_TESTIMONIAL_PARAM_ERROR),})}),
+                                @ExampleObject(name = "Example 1", summary = "Testimonial param Error", description = "when required fields are not filled in, sends a 400 (Bad Request) error message", value = SwaggerConstants.MODEL_TESTIMONIAL_PARAM_ERROR),})}),
         @ApiResponse(responseCode = "404", description = "Testimonial not found",
                 content = {
                     @Content(mediaType = "application/json",
                             schema = @Schema(implementation = MessageInfo.class),
                             examples = {
-                                @ExampleObject(name = "Example error 404", summary = "Testimonial not found", description = "When you enter the id of a nonexistent testimonial in the route, it sends a message error 400 (Not Found).", value = MODEL_TESTIMONIAL_ERROR_404
+                                @ExampleObject(name = "Example error 404", summary = "Testimonial not found", description = "When you enter the id of a nonexistent testimonial in the route, it sends a message error 400 (Not Found).", value = SwaggerConstants.MODEL_TESTIMONIAL_ERROR_404
                                 )})})})
     @PutMapping("/{id}")
     public ResponseEntity<TestimonialDto> update(@Parameter(description = "id of testimonial to be searched") @PathVariable Long id, @Validated @RequestBody TestimonialDto testimonialDto) {
@@ -108,21 +100,21 @@ public class TestimonialController {
                 @Content(mediaType = "application/json",
                         schema = @Schema(implementation = MessageInfo.class),
                         examples = {
-                            @ExampleObject(name = "Testimonial deleted", summary = "Testimonial deleted", description = "The testimonial is successfully removed when found by id, then a success message is sent.", value = MODEL_TESTIMONIAL_DELETE
+                            @ExampleObject(name = "Testimonial deleted", summary = "Testimonial deleted", description = "The testimonial is successfully removed when found by id, then a success message is sent.", value = SwaggerConstants.MODEL_TESTIMONIAL_DELETE
                             )})})
     @ApiResponse(responseCode = "400", description = "Error for the character entered",
             content = {
                 @Content(mediaType = "application/json",
                         schema = @Schema(implementation = MessageInfo.class),
                         examples = {
-                            @ExampleObject(name = "Example error 400", summary = "Error character", description = "When I enter an invalid character in the path it sends an error message 400 (Bad Request).", value = MODEL_TESTIMONIAL_ERROR_400
+                            @ExampleObject(name = "Example error 400", summary = "Error character", description = "When I enter an invalid character in the path it sends an error message 400 (Bad Request).", value = SwaggerConstants.MODEL_TESTIMONIAL_ERROR_400
                             )})})
     @ApiResponse(responseCode = "404", description = "Testimonial not found",
             content = {
                 @Content(mediaType = "application/json",
                         schema = @Schema(implementation = MessageInfo.class),
                         examples = {
-                            @ExampleObject(name = "Example error 404", summary = "Testimonial not found", description = "When you enter the id of a nonexistent testimonial in the route, it sends a message error 400 (Not Found).", value = MODEL_TESTIMONIAL_ERROR_404
+                            @ExampleObject(name = "Example error 404", summary = "Testimonial not found", description = "When you enter the id of a nonexistent testimonial in the route, it sends a message error 400 (Not Found).", value = SwaggerConstants.MODEL_TESTIMONIAL_ERROR_404
                             )})})
     @DeleteMapping("/{id}")
     public ResponseEntity<Map<String, String>> delete(@Parameter(description = "id of testimonial to be searched") @PathVariable Long id) {
--- src/main/java/com/alkemy/ong/dto/CommentBasicDto.java
+++ src/main/java/com/alkemy/ong/dto/CommentBasicDto.java
@@ -1,5 +1,6 @@
 package com.alkemy.ong.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -9,5 +10,6 @@ import lombok.Setter;
 @RequiredArgsConstructor
 
 public class CommentBasicDto {
+    @Schema(name = "body", example = "1", type = "String", description = "description: comment to show in the page")
     private String body;
 }
